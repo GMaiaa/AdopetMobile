091e0454b96aebb5837f22e448981be1f830dafc
FATEC ZONA LESTE <GMaiaa>
2024-05-09 10:23:23 -0300

    create signIn and signUp Layouts

---
+++ src/main/java/com/example/adopet/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.example.adopet
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+        setContentView(R.layout.sign_in_layout)
+
+    }
+}
---
+++ src/main/java/com/example/adopet/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.adopet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
+        setContentView(R.layout.sign_up_layout)
+
+    }
+}
